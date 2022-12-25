42a62b6445e8c8260efa6aac8f2be0b79bb35eca
Vinith M H <vinithmh2000@gmail.com>
2022-12-26 00:15:54 +0530

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+import notes from "../notes";
+
+
+function App(){
+    return <div>
+        <Header />
+            {notes.map( (noteItem) => <Note 
+key={noteItem.key}
+    title={noteItem.title}
+    content={noteItem.content}
+/>)
+}
+        <Footer />
+        
+    </div>
+}
+
+export default App;
